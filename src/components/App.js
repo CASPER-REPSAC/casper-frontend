3c934c8c38722338c00fe4bc242389263c5d2f7c
Nevaland <deanh0716@gmail.com>
2021-06-25 23:45:50 +0900

    Add home with router

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AppRouter from "components/Router"
+
+function App() {
+  return (
+    <AppRouter></AppRouter>
+  );
+}
+
+export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,6 @@
+const Home = () => {
+    return (
+        <div><p>1</p></div>
+    )
+}
+export default Home;
