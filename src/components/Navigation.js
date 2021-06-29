dabe0e562674e62cc768972d2e38d3cb8d90ece1
Nevaland <deanh0716@gmail.com>
2021-06-29 17:38:14 +0900

    Refactoring navigation with bootstrap

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,88 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Navigation = () => (
-  <nav className="navbar">
-    <div className="navbar__logo">
-      <Link to="/">CASPER</Link>
-    </div>
-
-    <ul className="navbar__menu">
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/board">Board</Link>
-        <ul>
-          <li>
-            <Link to="/board/notice">공지사항</Link>
-          </li>
-          <li>
-            <Link to="/board/free">자유게시판</Link>
-          </li>
-          <li>
-            <span>사진첩</span>
-          </li>
-          <li>
-            <span>블로그</span>
-          </li>
-          <li>
-            <span>준회원게시판</span>
-          </li>
-          <li>
-            <span>정회원게시판</span>
-          </li>
-          <li>
-            <span>졸업생게시판</span>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <Link to="/community">Community</Link>
-        <ul>
-          <li>
-            <Link to="/community/lobby">Lobby</Link>
-          </li>
-          <li>
-            <span>Study</span>
-          </li>
-          <li>
-            <span>Project</span>
-          </li>
-          <li>
-            <span>CTF</span>
-          </li>
-          <li>
-            <span>Activities</span>
-          </li>
-          <li>
-            <Link to="/community/sos">S.O.S</Link>
-          </li>
-          <li>
-            <Link to="/community/rank">RANK</Link>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <Link to="/library">Library</Link>
-        <ul>
-          <li>
-            <span>Presentations</span>
-          </li>
-          <li>
-            <span>Roadmap</span>
-          </li>
-          <li>
-            <a href="">CasperWIKI</a>
-          </li>
-          <li>
-            <a href="">NAS</a>
-          </li>
-        </ul>
-      </li>
-    </ul>
-
-    <div className="navbar__account"></div>
-  </nav>
+  <Navbar bg="light" expand="lg">
+    <Container>
+      <LinkContainer to="/">
+        <Navbar.Brand>CASPER</Navbar.Brand>
+      </LinkContainer>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/board">
+            <Nav.Link>Board</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/community">
+            <Nav.Link>Community</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/library">
+            <Nav.Link>Library</Nav.Link>
+          </LinkContainer>
+          <NavDropdown title="Neva" id="basic-nav-dropdown">
+            <NavDropdown.Item>동아리 토큰 인증</NavDropdown.Item>
+            <NavDropdown.Item>회원정보 보기</NavDropdown.Item>
+            <NavDropdown.Item>작성 글 보기</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item>로그아웃</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 );
 export default Navigation;
