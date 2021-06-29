419152cc702e3564e439d5097b9472223d2957b2
Nevaland <deanh0716@gmail.com>
2021-06-29 18:09:16 +0900

    Refactoring

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
