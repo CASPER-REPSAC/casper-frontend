d563894474ae7f48c6adca3a39b4ea7e32e3e80d
Nevaland <deanh0716@gmail.com>
2021-07-06 16:22:09 +0900

    Apply redux-persist

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,16 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import accountReducer from "modules/accountReducer";
 
 const rootReducer = combineReducers({
   accountReducer,
 });
 
-export default rootReducer;
+const persistConfig = {
+  key: "root",
+  storage: storage,
+};
+
+export default persistReducer(persistConfig, rootReducer);
